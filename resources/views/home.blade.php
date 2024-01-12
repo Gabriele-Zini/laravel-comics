ba6d071a273540e37954267e903108601e2a840e
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-01-12 17:08:01 +0100

    add: header wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,32 @@
+<header>
+    <div class="ms_upper-header text-uppercase d-flex justify-content-end container">
+        <div class="d-flex ms_font-size gap-5">
+            <p>dc power visa&reg;</p>
+            <p>additional dc sites</p>
+        </div>
+
+    </div>
+    <nav class="container">
+        <div class="d-flex justify-content-between align-items-center ">
+            <div class="text-center">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo molisana">
+            </div>
+            <ul class="d-flex justify-content-center gap-4 text-uppercase list-unstyled fw-bold">
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-primary" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</header>
