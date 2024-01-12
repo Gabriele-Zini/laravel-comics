fd7759cfad544ebe307c5d02689374aded49dd27
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-01-12 17:56:03 +0100

    add: ms_btn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <section class="ms_main-section">
         <div class="container">
+            <button class="ms_btn ms_middle-btn">current series</button>
             <div class="row aling-items-center">
                 @foreach ($comics as $comic)
                     <div class="col-12 col-md-3 col-lg-2 card ms_card">
@@ -11,6 +12,9 @@
                         <p class="mt-0">Series: {{ $comic['series'] }}</p>
                     </div>
                 @endforeach
+            </div>
+            <div class="d-flex justify-content-center">
+                <button class=" ms_btn">load more</button>
             </div>
         </div>
     </section>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,15 +1,16 @@
 <header>
-    <div class="ms_upper-header text-uppercase d-flex justify-content-end container">
-        <div class="d-flex ms_font-size gap-5">
-            <p>dc power visa&reg;</p>
-            <p>additional dc sites</p>
+    <div class="ms_upper-header p-1">
+        <div class="container text-uppercase d-flex justify-content-end">
+            <div class="d-flex ms_font-size gap-5 align-items-center">
+                <p class="m-0">dc power visa&reg;</p>
+                <p class="m-0">additional dc sites</p>
+            </div>
         </div>
-
     </div>
-    <nav class="container">
+    <nav class="container p-3">
         <div class="d-flex justify-content-between align-items-center ">
             <div class="text-center">
-                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo molisana">
+                <img class="ms_logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo molisana">
             </div>
             <ul class="d-flex justify-content-center gap-4 text-uppercase list-unstyled fw-bold">
                 <li>Characters</li>
@@ -29,4 +30,8 @@
             </form>
         </div>
     </nav>
+
+    <div class="jumbotron">
+    </div>
+
 </header>
